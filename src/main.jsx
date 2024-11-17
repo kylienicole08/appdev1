b200c8251ca7906c620aa691cbd4fc4bfb85c8cf
kylienicole08 <https://github.com/kylienicole08/personal-portfolio.git>
2024-11-18 00:21:33 +0800

    Done in Text Input

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Counter from './Counter.jsx'
 import ToggleButton from './ToggleButton.jsx'
+import TextInput from './TextInput.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Counter />
     <ToggleButton />
+    <TextInput />
   </StrictMode>,
 )
