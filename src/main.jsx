3395fe6f3bcce646dfc2ccb7274758513bd3c230
kylienicole08 <https://github.com/kylienicole08/personal-portfolio.git>
2024-11-18 00:19:40 +0800

    Done in Toggle Button

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Counter from './Counter.jsx'
+import ToggleButton from './ToggleButton.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Counter />
-    
+    <ToggleButton />
   </StrictMode>,
 )
